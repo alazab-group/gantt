b1fdbec864377b31929d47b7be82fd64b6c37865
Mohamed Azab <159525429+mohamedazab224@users.noreply.github.com>
2025-07-10 08:38:58 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gantt',
+    version='1.0.3',
+    description='Gantt App',
+    author='Your Name',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
